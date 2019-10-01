ca9742a536338b614b262fa92806bf9e473f1304
Darrin Lowery <lambda@monsto.com>
2019-10-01 17:17:09 -0500

    stop propagation

--- js/index.js
+++ js/index.js
@@ -10,9 +10,19 @@ const introtext = document.querySelector(".intro p");
 
 const sectHead = document.querySelectorAll(".text-content h2");
 const buttons = document.querySelectorAll(".btn");
+const destination = document.querySelectorAll(".destination");
+const foot = document.querySelector("footer");
+const body = document.querySelector("body");
+
+// footer
+body.addEventListener("click", e => {
+	e.target.style = `background-color: rgb(${rnd255()}, ${rnd255()}, ${rnd255()})`;
+});
+foot.addEventListener("click", e => {
+	e.target.style = `background-color: rgb(${rnd255()}, ${rnd255()}, ${rnd255()})`;
+});
 
 // welcome
-// welcome.addEventListener("mouseenter", e => {e.target.style = "margin-left: 50px"})
 welcome.addEventListener("mouseenter", e => {
 	chase(e.target);
 });
@@ -31,6 +41,7 @@ introtext.addEventListener("mousemove", e => {
 });
 introtext.addEventListener("click", e => {
 	e.target.style = `transform: none`;
+	e.stopPropagation();
 });
 
 // sectHead
@@ -46,22 +57,34 @@ sectHead.forEach(e1 => {
 	});
 });
 
-
 // buttons
 buttons.forEach(e1 => {
 	e1.addEventListener("mousemove", e2 => {
-		e2.target.style.transition = `all 0.5s`;
 		e2.target.style.textAlign = `center`;
-		e2.target.style.transform = `scale(${e2.offsetX * 0.05})`;
+		e2.target.style.color = `black`;
+		bgcolor(e2, "g");
+		bgcolor(e2, "b");
 	});
-	e1.addEventListener("mouseout", e2 => {
+	e1.addEventListener("click", e2 => {
 		e2.target.style.transform = `none`;
 		e2.target.style.textAlign = `left`;
 	});
 });
 
-
-
+// destination
+destination.forEach(e1 => {
+	e1.addEventListener("mousemove", e2 => {
+		e2.target.style.textAlign = `center`;
+		e2.target.style.color = `blue`;
+		bgcolor(e2, "g");
+		bgcolor(e2, "b");
+	});
+	e1.addEventListener("click", e2 => {
+		e2.target.style.color = `black`;
+		e2.target.style.textAlign = `left`;
+		e2.target.style.backgroundColor = `white`;
+	});
+});
 
 // -----------------
 // other crap
@@ -70,7 +93,6 @@ buttons.forEach(e1 => {
 function chase(e) {
 	welcome.tog ? (e.style = "text-align: right; ") : (e.style = "text-align: left");
 	welcome.tog = !welcome.tog;
-	// e.style = "margin-left: 50px"
 }
 
 function bgcolor(e, col) {
@@ -88,4 +110,6 @@ function bgcolor(e, col) {
 	}
 }
 
-clg(welcome.textContent);
+function rnd255() {
+	return Math.random() * Math.floor(255);
+}
